c680046978717730c6f11d87c84b64161df33931
ashpasa <asherpang03@gmail.com>
2025-08-21 08:57:09 +0800

    Rename chatbot and implement text output

---
+++ src/main/java/Alpha.java
@@ -0,0 +1,10 @@
+public class Alpha {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________\n"
+            + "Hello! I'm Alpha\n"
+            + "What can I do for you?\n"
+            + "____________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!\n"
+            + "____________________________________________________");
+    }
+}
