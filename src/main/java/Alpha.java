40800e916a452288b9809d18cbffd6a76377be38
ashpasa <asherpang03@gmail.com>
2025-08-26 15:13:29 +0800

    Add function to echo command

--- src/main/java/Alpha.java
+++ src/main/java/Alpha.java
@@ -1,10 +1,29 @@
+import java.util.Scanner;
+
 public class Alpha {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        int status = 1;
         System.out.println("____________________________________________________\n"
             + "Hello! I'm Alpha\n"
             + "What can I do for you?\n"
             + "____________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!\n"
+        do {
+            status = executeCommand();
+        } while (status != 0);
+    }
+
+    private static int executeCommand() {
+        String command = scanner.nextLine();
+        if (command.equals("bye")) {
+            System.out.println("____________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________");
+            return 0;
+        }
+        System.out.println("____________________________________________________\n"
+            + command + "\n"
             + "____________________________________________________");
+        return 1;
     }
 }
