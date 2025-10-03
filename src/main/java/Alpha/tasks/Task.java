82f8aaafaa730b4c928a7e8170105ff214609c34
ashpasa <asherpang03@gmail.com>
2025-10-04 00:15:58 +0800

    Implement search by string feature

--- src/main/java/Alpha/tasks/Task.java
+++ src/main/java/Alpha/tasks/Task.java
@@ -40,4 +40,8 @@ public abstract class Task {
         }
         return typeIndicator + (isDone ? "[X] " : "[O] ") + description;
     }
+
+    public String getDescription() {
+        return description;
+    }
 }
