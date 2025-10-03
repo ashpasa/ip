fbd21452e9482ee3c8cf6f4fda92d2ab2523332c
ashpasa <asherpang03@gmail.com>
2025-10-03 14:59:16 +0800

    Refactor main function

--- src/main/java/Alpha/Alpha.java
+++ src/main/java/Alpha/Alpha.java
@@ -1,23 +1,32 @@
 import java.io.FileNotFoundException;
 
 public class Alpha {
-    static Ui ui = new Ui();
-    static TaskList taskList = new TaskList();
-    static Storage storage = new Storage("data/taskList.txt");
-    
-    public static void main(String[] args) {
-        ui.sendWelcomeMessage();
+    static Ui ui;
+    static TaskList taskList;
+    static Storage storage;
+
+    public Alpha(String filePath) {
+        ui = new Ui();
+        taskList = new TaskList();
+        storage = new Storage(filePath);
         try {
             storage.readTasksFromFile(ui, taskList);
-            ui.sendMessage("Ooh, you have a saved task list!"
-                + System.lineSeparator()
-                + "Let me get it for you...");
+            System.out.println("Saved task list found! Loading tasks...");
         } catch (FileNotFoundException e) {
             System.out.println("It seems you don't have any saved tasks, let me create a new file for you...");
         }
+        return;
+    }
+
+    public void run() {
+        ui.sendWelcomeMessage();
         while (true) {
             String command = ui.readCommand();
             Parser.carryOutCommand(ui, taskList, storage, command);
         }
+    }
+
+    public static void main(String[] args) {
+        new Alpha("data/taskList.txt").run();
     }
 }
